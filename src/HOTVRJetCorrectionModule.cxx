e5820e1ef0bdb8c5001c8d60ef573a8ea3eddad7
Ksenia de Leo <ksenia.de.leo@desy.de>
2022-02-03 16:50:19 +0100

    update JEC for UL16

--- src/HOTVRJetCorrectionModule.cxx
+++ src/HOTVRJetCorrectionModule.cxx
@@ -20,8 +20,11 @@ HOTVRJetCorrectionModule::HOTVRJetCorrectionModule(Context & ctx) {
   string jec_tag_2018 = "Autumn18";
   string jec_ver_2018 = "19";
 
-  // string jec_tag_UL16 = ""; // TODO
-  // string jec_ver_UL16 = ""; // TODO
+  string jec_tag_UL16preVFP = "Summer19UL16APV"; 
+  string jec_ver_UL16preVFP = "7"; 
+
+  string jec_tag_UL16postVFP = "Summer19UL16"; 
+  string jec_ver_UL16postVFP = "7"; 
 
   string jec_tag_UL17 = "Summer19UL17";
   string jec_ver_UL17 = "5";
@@ -42,7 +45,8 @@ HOTVRJetCorrectionModule::HOTVRJetCorrectionModule(Context & ctx) {
       jet_corrector_MC->setup2016(std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesMC(jec_tag_2016, jec_ver_2016, jec_jet_coll),"hotvr_subjets"));
       jet_corrector_MC->setup2017(std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesMC(jec_tag_2017, jec_ver_2017, jec_jet_coll),"hotvr_subjets"));
       jet_corrector_MC->setup2018(std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesMC(jec_tag_2018, jec_ver_2018, jec_jet_coll),"hotvr_subjets"));
-      // jet_corrector_MC->setupUL16(std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesMC(jec_tag_UL16, jec_ver_UL16, jec_jet_coll),"hotvr_subjets"));
+      jet_corrector_MC->setupUL16preVFP(std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesMC(jec_tag_UL16preVFP, jec_ver_UL16preVFP, jec_jet_coll),"hotvr_subjets"));
+      jet_corrector_MC->setupUL16postVFP(std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesMC(jec_tag_UL16postVFP, jec_ver_UL16postVFP, jec_jet_coll),"hotvr_subjets"));
       jet_corrector_MC->setupUL17(std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesMC(jec_tag_UL17, jec_ver_UL17, jec_jet_coll),"hotvr_subjets"));
       jet_corrector_MC->setupUL18(std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesMC(jec_tag_UL18, jec_ver_UL18, jec_jet_coll),"hotvr_subjets"));
     }
@@ -63,10 +67,15 @@ HOTVRJetCorrectionModule::HOTVRJetCorrectionModule(Context & ctx) {
         jec_switcher_18->setupRun(runItr, std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesDATA(jec_tag_2018, jec_ver_2018, jec_jet_coll, runItr),"hotvr_subjets"));
       }
 
-      // jec_switcher_UL16.reset(new RunSwitcher(ctx, "2016"));
-      // for (const auto & runItr : runPeriods2016) { // runPeriods defined in common/include/Utils.h
-      //   jec_switcher_UL16->setupRun(runItr, std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesDATA(jec_tag_UL16, jec_ver_UL16, jec_jet_coll, runItr),"hotvr_subjets"));
-      // }
+      jec_switcher_UL16preVFP.reset(new RunSwitcher(ctx, "2016"));
+      for (const auto & runItr : runPeriodsUL16preVFP) { 
+        jec_switcher_UL16preVFP->setupRun(runItr, std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesDATA(jec_tag_UL16preVFP, jec_ver_UL16preVFP, jec_jet_coll, runItr),"hotvr_subjets"));
+      }
+
+      jec_switcher_UL16postVFP.reset(new RunSwitcher(ctx, "2016"));
+      for (const auto & runItr : runPeriodsUL16postVFP) { 
+        jec_switcher_UL16postVFP->setupRun(runItr, std::make_shared<GenericJetCorrector>(ctx, JERFiles::JECFilesDATA(jec_tag_UL16postVFP, jec_ver_UL16postVFP, jec_jet_coll, runItr),"hotvr_subjets"));
+      }
 
       jec_switcher_UL17.reset(new RunSwitcher(ctx, "2017"));
       for (const auto & runItr : runPeriods2017) {
@@ -82,7 +91,8 @@ HOTVRJetCorrectionModule::HOTVRJetCorrectionModule(Context & ctx) {
       jet_corrector_data->setup2016(jec_switcher_16);
       jet_corrector_data->setup2017(jec_switcher_17);
       jet_corrector_data->setup2018(jec_switcher_18);
-      // jet_corrector_data->setupUL16(jec_switcher_UL16);
+      jet_corrector_data->setupUL16preVFP(jec_switcher_UL16preVFP);
+      jet_corrector_data->setupUL16postVFP(jec_switcher_UL16postVFP);
       jet_corrector_data->setupUL17(jec_switcher_UL17);
       jet_corrector_data->setupUL18(jec_switcher_UL18);
     }
@@ -97,8 +107,10 @@ HOTVRJetCorrectionModule::HOTVRJetCorrectionModule(Context & ctx) {
     jer_tag = "Fall17_V3";
   } else if (year == Year::is2018) {
     jer_tag = "Autumn18_V7";
-  // } else if (year == Year::isUL16) {
-  //   jer_tag = ""; // TODO
+  } else if (year == Year::isUL16preVFP) {
+    jer_tag = "Summer20UL16APV_JRV3";
+  } else if (year == Year::isUL16postVFP) {
+    jer_tag = "Summer20UL16_JRV3";
   } else if (year == Year::isUL17) {
     jer_tag = "Summer19UL17_JRV2";
   } else if (year == Year::isUL18) {
